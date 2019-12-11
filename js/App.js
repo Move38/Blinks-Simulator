0ab20ce0dd32b992570e38c8fb5dfcbd283824d0
XY Feng <fxy0111@gmail.com>
2019-12-11 17:37:18 -0500

    add vector functions through dcomp, add free rotation and snap to new rotation function

--- js/App.js
+++ js/App.js
@@ -1,4 +1,5 @@
 var BLOCK_RADIUS = 24;
+var TOTAL_BLOCK = 6;
 
 var engine;
 var mouseConstraints;
@@ -16,8 +17,12 @@ function setup() {
     Matter.Engine.run(engine);
 
     // add hexgons
-    blocks.push(generateBlock(width/2, height/2, BLOCK_RADIUS * 2));
-    blocks.push(generateBlock(width/2, height/2, BLOCK_RADIUS * 2));
+    for(var i = 0; i < TOTAL_BLOCK; i ++){
+        // generate a new block and keep them centered
+        var alt = i % 2 * 2 - 1; // -1 or 1
+        var block = generateBlock(width/2 + (i - TOTAL_BLOCK/2)*BLOCK_RADIUS * 1.732, height/2 + alt*BLOCK_RADIUS*1.5, BLOCK_RADIUS * 2);
+        blocks.push(block);
+    }
     console.log(blocks[0]);
 
     // add walls
@@ -64,10 +69,7 @@ function generateBlock(x, y, s){
     var block = Matter.Bodies.polygon(x, y, 6, s, { 
         friction: 0.8,
         frictionAir: 0.8,
-        inertia: Infinity, //disable rotation
-        chamfer: {
-            radius: 10 //rounded corner
-        }
+        // inertia: Infinity, //enable rotation
     });
     Matter.World.addBody(engine.world, block);
     return block;
@@ -76,22 +78,39 @@ function generateBlock(x, y, s){
 function drawBlocks(){
     for(var i=0; i<blocks.length; i++){
         var one = blocks[i];
-        // draw stroke
+        // update round radius
+        var vertices = Matter.Vertices.chamfer(one.vertices, 10, -1, 2, 14); //default chamfer
+        // draw block
         noStroke();
         fill(255, 240);
         beginShape();
-        for(var j=0; j<one.vertices.length; j++ ){
-            var ver = one.vertices[j];
+        for(var j=0; j<vertices.length; j++ ){
+            var ver = vertices[j];
             vertex(ver.x, ver.y);
         }
         endShape(CLOSE);
 
         // draw angle indicator
+        var midPoint = {
+            x: one.vertices[0].x/2 + one.vertices[one.vertices.length-1].x/2, 
+            y: one.vertices[0].y/2 + one.vertices[one.vertices.length-1].y/2
+        }
         stroke(255, 0, 0);
         line(one.position.x, 
             one.position.y, 
-            one.vertices[0].x/2 + one.vertices[one.vertices.length-1].x/2, 
-            one.vertices[0].y/2 + one.vertices[one.vertices.length-1].y/2);
+            one.position.x + (midPoint.x - one.position.x) * 0.88,
+            one.position.y + (midPoint.y - one.position.y) * 0.88
+        );
+            
+        // draw axes
+        noStroke();
+        fill(0, 0, 255);
+        beginShape();
+        for(var k=0; k<one.axes.length; k++ ){
+            var ver = one.axes[k];
+            vertex(ver.x, ver.y);
+        }
+        endShape(CLOSE);
     }
 }
 
@@ -112,6 +131,12 @@ function endDrag(){
             x: target_shadow.body.position.x + target_shadow.offset.x,
             y: target_shadow.body.position.y + target_shadow.offset.y
         })
+        // limit to 0 to 60
+        var angleDiff = degrees(target_shadow.body.angle - curr_dragging.angle + PI * 2) % 60
+        // update to -30 to 30 for minimal rotation
+        angleDiff = angleDiff > 30 ? angleDiff - 60 : angleDiff;
+        var angle = (curr_dragging.angle + radians(angleDiff))
+        Matter.Body.setAngle(curr_dragging, angle);
         clearTargetShadow();
     }
 }
@@ -122,10 +147,25 @@ function checkLocations(){
         if(one.id !== curr_dragging.id){
             var d = dist(curr_dragging.position.x, curr_dragging.position.y, one.position.x, one.position.y);
             if(d < BLOCK_RADIUS * 5) {
-                // draw target location
-                var angleDeg = degrees(atan2(curr_dragging.position.y - one.position.y, curr_dragging.position.x - one.position.x));
-                var targetDeg = int(((angleDeg + 360 + 30) % 360) / 60);
-                updateTargetShadow(one, targetDeg);
+                var p1 = [one.position.x, one.position.y];
+                var p2 = [curr_dragging.position.x, curr_dragging.position.y];
+                var pInter;
+                for(var j=0; j<=one.vertices.length; j++){
+                    var vert1 = one.vertices[j];
+                    var vert2 = one.vertices[(j + 1) % 6];
+                    var q1 = [vert1.x, vert1.y];
+                    var q2 = [vert2.x, vert2.y];
+                    var lineIntersect = decomp.lineSegmentsIntersect(p1, p2, q1, q2);
+                    if(lineIntersect){
+                        pInter = {
+                            x: (vert1.x + vert2.x) / 2,
+                            y: (vert1.y + vert2.y) / 2
+                        }
+                        break;
+                    }
+                }
+                updateTargetShadow(one, pInter);
+                break;
             }
             else {
                 clearTargetShadow();
@@ -138,10 +178,10 @@ function clearTargetShadow() {
     target_shadow = null;
 }
 
-function updateTargetShadow(body, deg){
-    var offsetX = cos(radians(deg * 60)) * BLOCK_RADIUS * 3.3334;
-    var offsetY = sin(radians(deg * 60)) * BLOCK_RADIUS * 3.3334;
-    // console.log('draw target location', body, deg, offsetX, offsetY);
+function updateTargetShadow(body, p){
+    var offsetX = (p.x - body.position.x) * 2;
+    var offsetY = (p.y - body.position.y) * 2;
+    // console.log('draw target location', body, p, offsetX, offsetY);
     target_shadow = {
         body: body,
         offset: {
@@ -161,11 +201,13 @@ function updateTargetShadow(body, deg){
 
 function drawTargetShadow(){
     if(!target_shadow) return;
+    // update round radius
+    var vertices = Matter.Vertices.chamfer(target_shadow.vertices, 10, -1, 2, 14); //default chamfer
     noStroke();
     fill(60);
     beginShape();
-    for(var i=0; i<target_shadow.vertices.length; i++ ){
-        var ver = target_shadow.vertices[i];
+    for(var i=0; i<vertices.length; i++ ){
+        var ver = vertices[i];
         vertex(ver.x, ver.y);
     }
     endShape(CLOSE);
