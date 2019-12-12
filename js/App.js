38b5aad71c82c4339cd1a6d634b0d5dc757e13c1
XY Feng <fxy0111@gmail.com>
2019-12-11 19:39:36 -0500

    add block cluster detection

--- js/App.js
+++ js/App.js
@@ -6,6 +6,7 @@ var mouseConstraints;
 var curr_dragging;
 var target_shadow; 
 var blocks = [];
+var groups = [];
 
 function setup() {
     var canvas = createCanvas(windowWidth, windowHeight);
@@ -23,6 +24,7 @@ function setup() {
         var block = generateBlock(width/2 + (i - TOTAL_BLOCK/2)*BLOCK_RADIUS * 1.732, height/2 + alt*BLOCK_RADIUS*1.5, BLOCK_RADIUS * 2);
         blocks.push(block);
     }
+    updateGroups();
     console.log(blocks[0]);
 
     // add walls
@@ -44,18 +46,34 @@ function setup() {
         mouseConstraints
     );
     
+    Matter.Events.on(mouseConstraints, 'mousedown', function(){
+        console.log('mouse down', mouseConstraints.body);
+        if(mouseConstraints.body){
+            Matter.Body.setStatic(mouseConstraints.body, false);
+        }
+    });
+    
     Matter.Events.on(mouseConstraints, 'startdrag', function(){
+        // console.log('start drag', mouseConstraints.body);
         curr_dragging = mouseConstraints.body;
         startDrag();
     });
     Matter.Events.on(mouseConstraints, 'mousemove', function(){
+        // console.log('mouse move');
         if(curr_dragging){
             duringDrag();
         }
     });
     Matter.Events.on(mouseConstraints, 'enddrag', function(){
-        endDrag();
-        curr_dragging = null;
+        if(curr_dragging){
+            console.log('end drag', curr_dragging);
+            Matter.Body.setStatic(curr_dragging, true);
+            endDrag();
+            curr_dragging = null;
+        }
+    });
+    Matter.Events.on(mouseConstraints, 'mouseup', function(){
+        console.log('mouse up');
     });
 }
 
@@ -65,16 +83,7 @@ function draw() {
     drawBlocks();
 }
 
-function generateBlock(x, y, s){
-    var block = Matter.Bodies.polygon(x, y, 6, s, { 
-        friction: 0.8,
-        frictionAir: 0.8,
-        // inertia: Infinity, //enable rotation
-    });
-    Matter.World.addBody(engine.world, block);
-    return block;
-}
-
+/* RENDER */
 function drawBlocks(){
     for(var i=0; i<blocks.length; i++){
         var one = blocks[i];
@@ -114,7 +123,21 @@ function drawBlocks(){
     }
 }
 
-// Events
+function drawTargetShadow(){
+    if(!target_shadow) return;
+    // update round radius
+    var vertices = Matter.Vertices.chamfer(target_shadow.vertices, 10, -1, 2, 14); //default chamfer
+    noStroke();
+    fill(60);
+    beginShape();
+    for(var i=0; i<vertices.length; i++ ){
+        var ver = vertices[i];
+        vertex(ver.x, ver.y);
+    }
+    endShape(CLOSE);
+}
+
+/* EVENTS */
 function startDrag(){
     // console.log('start drag', curr_dragging);
 }
@@ -139,8 +162,11 @@ function endDrag(){
         Matter.Body.setAngle(curr_dragging, angle);
         clearTargetShadow();
     }
+    updateGroups();
 }
 
+
+/* TARGET SHADOW */
 function checkLocations(){
     // get the closest block
     var minDist = windowWidth;
@@ -205,16 +231,91 @@ function updateTargetShadow(body, p){
     }
 }
 
-function drawTargetShadow(){
-    if(!target_shadow) return;
-    // update round radius
-    var vertices = Matter.Vertices.chamfer(target_shadow.vertices, 10, -1, 2, 14); //default chamfer
-    noStroke();
-    fill(60);
-    beginShape();
-    for(var i=0; i<vertices.length; i++ ){
-        var ver = vertices[i];
-        vertex(ver.x, ver.y);
+
+/* GROUPS */
+function updateGroups(){
+     //reset groups
+    groups = [];
+    for(var r = 0; r < blocks.length; r++) {
+        blocks[r].group = 0;
     }
-    endShape(CLOSE);
+    // loop
+    for(var i = 0; i < blocks.length; i++) {
+        for(var j = i+1; j < blocks.length; j++) {
+            var b1 = blocks[i];
+            var b2 = blocks[j];
+            var d = dist(b1.position.x, b1.position.y, b2.position.x, b2.position.y);
+            if( d < BLOCK_RADIUS * 3.47 ){
+                if(b1.group === 0 && b2.group === 0){
+                    b1.group = b2.group = groups.length + 1;
+                    groups.push([b1.id, b2.id]);
+                }
+                else if(b1.group === 0){
+                    b1.group = b2.group;
+                    addToGroups(b1.id, b2.id);
+                }
+                else if(b2.group === 0) {
+                    b2.group = b1.group;
+                    addToGroups(b2.id, b1.id);
+                }
+                else if(b1.group !== b2.group){
+                    // merge groups
+                    if(b1.group < b2.group){
+                        mergeGroups(b2.group, b1.group);
+                    }
+                    else {
+                        mergeGroups(b1.group, b2.group);
+                    }
+                }
+            }
+        }
+    }
+    // console.log('Groups:', groups);
+}
+
+function addToGroups(from, to){
+    // console.log('add: ', from, to);
+    for(var i = 0; i < groups.length; i ++){
+        if(groups[i].includes(to)) {
+            groups[i].push(from);
+            break;
+        }
+    }
+}
+
+function mergeGroups(from, to) {
+    // console.log('merge groups: ', groups, from, to);
+    for(var i = 0; i < groups[from - 1].length; i ++) {
+        var id = groups[from - 1][i];
+        var block = getBlockFromID(id);
+        if(!block) {
+            console.warn('unable to locate block based on ID');
+        }
+        block.group = to;
+        groups[to - 1].push(id);
+    }
+    groups.splice(from - 1, 1); 
+}
+
+
+/* BLOCKS */
+
+function getBlockFromID(id) {
+    for(var i = 0; i < blocks.length; i++) {
+        if(blocks[i].id === id) {
+            return blocks[i];
+        }
+    }
+    return null;
+}
+
+function generateBlock(x, y, s){
+    var block = Matter.Bodies.polygon(x, y, 6, s, { 
+        friction: 0.8,
+        frictionAir: 0.8,
+        isStatic: true
+    });
+    block.group = 0;
+    Matter.World.addBody(engine.world, block);
+    return block;
 }
