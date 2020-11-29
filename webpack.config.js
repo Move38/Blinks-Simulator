060193be48e954ee0269d0b982f7796c16f23501
XY Feng <fxy0111@gmail.com>
2020-11-28 23:26:10 -0500

    fixed webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'blinks.js',
-        path: path.resolve(__dirname, 'build'),
-        pathinfo: false,
-        library: 'blinks',
-        libraryTarget: "umd",
-    },
-    devServer: {
-        contentBase: [
-            path.resolve(__dirname, './examples'),
-            path.resolve(__dirname, './build')
-        ],
-        compress: true,
-        port: 8000
-    },
+module.exports = (env, arg) => {
+    const isProd = arg.mode === 'production'
+
+    return {
+        mode: 'development',
+        entry: './src/index.js',
+        output: {
+            filename: `blinks${isProd ? '.min' : ''}.js`,
+            path: path.resolve(__dirname, 'build'),
+            pathinfo: false,
+            library: 'blinks',
+            libraryTarget: "umd",
+        },
+        optimization: { minimize: isProd },
+        devServer: {
+            contentBase: [
+                path.resolve(__dirname, './examples'),
+                path.resolve(__dirname, './build')
+            ],
+            compress: true,
+            port: 8000
+        }
+    }
 };
